daeafd49db2a2966893d8e281091f13cd3b2be0f
ariefdpras <ariefprasetyo1994@gmail.com>
2025-03-17 00:15:30 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// //Hover Navbar2
+
+// //Brand
+// const brand = document.querySelector("#brand");
+// const btnBrand = document.querySelector("#brand-btn");
+
+// // btnBrand.addEventListener("mouseenter", function () {
+// //   brand.classList.add("flex");
+// //   brand.classList.remove("hidden");
+// //   e.preventDefault();
+// // });
+
+// document.querySelector("#brand-btn").onmouseover = (e) => {
+//   brand.classList.add("flex");
+//   brand.classList.remove("hidden");
+//   e.preventDefault();
+// };
+
+// document.addEventListener("mouseleave", function (e) {
+//   if (!btnBrand.contains(e.target) && !brand.contains(e.target)) {
+//     navbarNav.classList.remove("flex");
+//     brand.classList.add("hidden");
+//   }
+// });
+
+// // brand.addEventListener("mouseleave", function (e) {
+// //   brand.classList.add("hidden");
+// //   brand.classList.remove("flex");
+// //   e.preventDefault();
+// // });
+
+// //Industry
+// const industry = document.querySelector("#industry");
+// const btnIndustry = document.querySelector("#industry-btn");
+
+// btnIndustry.addEventListener("mouseenter", function (e) {
+//   industry.classList.add("flex");
+//   industry.classList.remove("hidden");
+//   e.preventDefault();
+// });
+
+// industry.addEventListener("mouseleave", function (e) {
+//   industry.classList.add("hidden");
+//   industry.classList.remove("flex");
+// });
+
+// const application = document.querySelector(".container-application");
